721a4fb555b7c0519cb4d16f326283d6573719f2
eMobilis <simonlaban453@gmail.com>
2023-06-26 09:17:15 +0300

    simple area calculator

--- calc_area.py
+++ calc_area.py
@@ -1,7 +1,9 @@
+#enable user to enter input
 width = input("Enter width")
 length = input("Enter length")
+#convert two variables into integer
 Width = int(width)
 Length = int(length)
-
+#perform calculation
 Area = (Width * Length)
-print(Area)
+print("Area is", Area)
