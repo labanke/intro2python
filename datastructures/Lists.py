0c7f3cb0aee61b7d799e5535b7712c1d6481330b
eMobilis <simonlaban453@gmail.com>
2023-06-29 09:53:00 +0300

    Lists in python

---
+++ datastructures/Lists.py
@@ -0,0 +1,9 @@
+#defined using squared brackets
+#you can modify data - add, remove or update data in a list
+#a list has order
+
+friends = ["Bob", "Marie", "Sue"]
+friends.append("Roy")
+friends.append("Gill")
+friends.remove("Bob")
+print(friends)
