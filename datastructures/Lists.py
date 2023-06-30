69c5ad89d9a689967f5f2037a45b44b98def7ddd
eMobilis <simonlaban453@gmail.com>
2023-06-30 08:57:48 +0300

    Set data structure

--- datastructures/Lists.py
+++ datastructures/Lists.py
@@ -2,10 +2,11 @@
 #you can modify data - add, remove or update data in a list
 #a list has order
 
-friends = ["Bob", "Marie", "Sue"]
-#friends.append("Roy")
-#friends.append("Gill")
+friends = ["Bob", "Marie", "Sue", "Ann", "Kim"]
+friends.append("Roy")
+friends.append("Gill")
 #friends.remove("Sue")
 print(friends)
 
-print(friends[0])
+print(friends[4])
+print(friends[1:])
---
+++ datastructures/sets.py
@@ -0,0 +1,7 @@
+#defined by the curly brackets
+#order is not guaranteed
+#does not allow duplicates
+
+myfriends = {"Joe", "Mike", "Lyn", "Essy"}
+myfriends.add("Joe")
+print(myfriends)
