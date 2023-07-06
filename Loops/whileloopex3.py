ea5ae6563bd25606ced34b374853774223ac4a9a
eMobilis <simonlaban453@gmail.com>
2023-07-06 11:10:06 +0300

    FizzBuzz While

--- Loops/whileloopex3.py
+++ Loops/whileloopex3.py
@@ -1,6 +1,11 @@
-x = 1
-while x <= 100:
-    print(x)
-    x += 1
-    if x % 3 == "Fizz" and x % 5 == "Buzz":
-        print()
+i = 1
+v = 100
+for x in range (i, v + 1):
+    if x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    elif(x % 3 == 0) and (x % 5 == 0):
+        print("FizzBuzz")
+    else:
+        print(x)
