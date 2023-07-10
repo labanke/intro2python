6760caa2e7cc17cee0cdc1bda044bea2375d02cc
eMobilis <simonlaban453@gmail.com>
2023-07-10 10:16:37 +0300

    Polymorphism

---
+++ OOP/polymorphism.py
@@ -0,0 +1,16 @@
+#taking many forms
+
+y = "Hello my people"
+print(len(y))
+
+
+student = ("anne", "jim", "Fredy")
+print(len(student))
+
+myvehicles = {
+    "brand":"Nissan",
+    "model": "Teana",
+    "YOM": "2020"
+    
+}
+print(len(myvehicles))
